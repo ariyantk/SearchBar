d78fc65cd37f679526f96485f19d3f6a353b8302
ariyantk <ariyantk@gmail.com>
2023-08-10 19:03:48 +0330

    new update for Json

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Master's List
-let person = [
+/*let person = [
   {
     name: "name1",
     job: "سازتدریسی: سنتور، تار، سه تار",
@@ -40,15 +40,22 @@ let person = [
     job: "سازتدریسی: پیانو",
     image: "",
   },
-];
+];*/
+
+let person;
 
 //Make a search bar
 const userCardTemplate = document.querySelector("[data-user-template]");
 const userCardContainer = document.querySelector("[data-user-cards-container]");
 const searchInput = document.querySelector("[data-search]");
+const categoryTemplate = document.querySelector("[category-template]");
+const dataCategoryContainer = document.querySelector(
+  "[data-category-container]"
+);
 
 //array for mapping users
 let users = [];
+let categories = [];
 
 //Get input and check it
 searchInput.addEventListener("input", (e) => {
@@ -105,6 +112,33 @@ dropDowns.forEach((dropDown) => {
 });
 
 //Mapping users
+fetch("./masters.json")
+  .then((res) => res.json())
+  .then((data) => {
+    users = data.map((user) => {
+      const card = userCardTemplate.content.cloneNode(true).children[0];
+      const header = card.querySelector("[data-header]");
+      const body = card.querySelector("[data-body]");
+      header.textContent = user.name;
+      body.textContent = user.job;
+      userCardContainer.append(card);
+      return { name: user.name, job: user.job, Element: card };
+    });
+  });
+
+//mapping categories
+fetch("./category.json")
+  .then((res) => res.json())
+  .then((data) => {
+    categories = data.map((cat) => {
+      const title = categoryTemplate.content.cloneNode(true).children[0];
+      const dataList = title.querySelector("[data-list]");
+      dataList.textContent = cat.instrumentName;
+      dataCategoryContainer.append(title);
+      return { title: cat.instrumentName, Element: title };
+    });
+  });
+/* 
 users = person.map((user) => {
   const card = userCardTemplate.content.cloneNode(true).children[0];
   const header = card.querySelector("[data-header]");
@@ -114,3 +148,4 @@ users = person.map((user) => {
   userCardContainer.append(card);
   return { name: user.name, job: user.job, Element: card };
 });
+*/
