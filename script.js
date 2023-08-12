2c9f002df3e90c57ee2427f792716e3f6315477e
ariyantk <ariyantk@gmail.com>
2023-08-13 01:45:49 +0330

    Shit things Fixed
    add some images and fixed filter menu

--- script.js
+++ script.js
@@ -24,49 +24,12 @@ searchInput.addEventListener("input", (e) => {
   });
 });
 
-//mapping categories
-fetch("./category.json")
-  .then((res) => res.json())
-  .then((data) => {
-    categories = data.map((cat) => {
-      const title = categoryTemplate.content.cloneNode(true).children[0];
-      const dataList = title.querySelector("[data-list]");
-      dataList.textContent = cat.instrumentName;
-      title.addEventListener("click", () => {
-        const options = document.querySelectorAll(".menu .innerlist li");
-        const selected = document.querySelector(".selected");
-        selected.innerText = cat.instrumentName;
-        const newInstrument = cat.instrumentName;
-        users.forEach((user) => {
-          if (newInstrument == "همه") {
-            location.reload();
-          }
-          const isVisible = user.job.includes(newInstrument);
-          user.Element.classList.toggle("hide", !isVisible);
-        });
-        options.forEach((opt) => {
-          opt.classList.remove("active");
-        });
-        title.classList.add("active");
-      });
-      dataCategoryContainer.append(title);
-      return { title: cat.instrumentName, Element: title };
-    });
-  });
-
-//DropDown menu
 const dropDowns = document.querySelectorAll(".dropdown");
 
 dropDowns.forEach((dropDown) => {
   const select = dropDown.querySelector(".select");
-  console.log(select);
   const caret = dropDown.querySelector(".caret");
   const menu = dropDown.querySelector(".menu");
-  //const options = dropDown.querySelectorAll(".menu li");
-  const options = dropDown.querySelectorAll(".menu .innerlist li");
-  //console.log(options);
-  const selected = dropDown.querySelector(".selected");
-  //console.log(selected);
 
   select.addEventListener("click", () => {
     select.classList.toggle("select-cliked");
@@ -74,31 +37,41 @@ dropDowns.forEach((dropDown) => {
     menu.classList.toggle("menu-open");
   });
 
-  options.forEach((option) => {
-    option.addEventListener("click", () => {
-      selected.innerText = option.innerText;
+  //mapping categories
+  fetch("./category.json")
+    .then((res) => res.json())
+    .then((data) => {
+      categories = data.map((cat) => {
+        const title = categoryTemplate.content.cloneNode(true).children[0];
+        const dataList = title.querySelector("[data-list]");
+        dataList.textContent = cat.instrumentName;
 
-      //search person with selected job
-      const newInstrument = selected.innerText;
-      users.forEach((user) => {
-        if (newInstrument == "همه") {
-          location.reload();
-        }
-        const isVisible = user.job.includes(newInstrument);
-        user.Element.classList.toggle("hide", !isVisible);
+        title.addEventListener("click", () => {
+          const options = document.querySelectorAll(".menu .innerlist li");
+          const selected = document.querySelector(".selected");
+          selected.innerText = cat.instrumentName;
+          const newInstrument = cat.instrumentName;
+          users.forEach((user) => {
+            if (newInstrument == "همه") {
+              location.reload();
+            }
+            const isVisible = user.job.includes(newInstrument);
+            user.Element.classList.toggle("hide", !isVisible);
+            menu.classList.remove("menu-open");
+          });
+
+          options.forEach((opt) => {
+            opt.classList.remove("active");
+          });
+
+          title.classList.add("active");
+          title.classList.remove("active");
+        });
+
+        dataCategoryContainer.append(title);
+        return { title: cat.instrumentName, Element: title };
       });
-
-      select.classList.remove("select-cliked");
-      caret.classList.remove("caret-rotate");
-      menu.classList.remove("move-open");
-
-      options.forEach((option) => {
-        option.classList.remove("active");
-      });
-
-      option.classList.add("active");
     });
-  });
 });
 
 //Mapping users
@@ -109,10 +82,11 @@ fetch("./masters.json")
       const card = userCardTemplate.content.cloneNode(true).children[0];
       const header = card.querySelector("[data-header]");
       const body = card.querySelector("[data-body]");
+      const image = card.querySelector("#profile");
       header.textContent = user.name;
       body.textContent = user.job;
+      image.setAttribute("src", user.image);
       userCardContainer.append(card);
       return { name: user.name, job: user.job, Element: card };
     });
   });
-
